479f3c99d4ca0004d66494356b367477ec3b5d6a
werthmannsupercode <werthmannsupercode@gmail.com>
2022-04-28 13:28:23 +0200

    first try

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+let box1 = document.getElementById('box1');
+let box2 = document.getElementById('box2');
+let box3 = document.getElementById('box3');
+let box4 = document.getElementById('box4');
+let box5 = document.getElementById('box5');
+let box6 = document.getElementById('box6');
+let box7 = document.getElementById('box7');
+let box8 = document.getElementById('box8');
+let box9 = document.getElementById('box9');
+
+function showArticle() {
+    box1.style.height = '200px';
+}
+
+function hideArticle() {
+    box1.style.height = 0;
+}
+
+function showArticle2() {
+    box2.style.height = '200px';
+}
+
+function hideArticle2() {
+    box2.style.height = 0;
+}
+
+function showArticle3() {
+    box3.style.height = '200px';
+}
+
+function hideArticle3() {
+    box3.style.height = 0;
+}
+
+function showArticle4() {
+    box4.style.height = '200px';
+}
+
+function hideArticle4() {
+    box4.style.height = 0;
+}
+
+function showArticle5() {
+    box5.style.height = '200px';
+}
+
+function hideArticle5() {
+    box5.style.height = 0;
+}
+
+function showArticle6() {
+    box6.style.height = '200px';
+}
+
+function hideArticle6() {
+    box6.style.height = 0;
+}
+
+function showArticle7() {
+    box7.style.height = '200px';
+}
+
+function hideArticle7() {
+    box7.style.height = 0;
+}
+
+function showArticle8() {
+    box8.style.height = '200px';
+}
+
+function hideArticle8() {
+    box8.style.height = 0;
+}
+
+function showArticle9() {
+    box9.style.height = '200px';
+}
+
+function hideArticle9() {
+    box9.style.height = 0;
+}
